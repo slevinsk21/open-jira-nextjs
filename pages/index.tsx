74059d3cc364bc95f6b628afe0112fab7c2af723
Leonel Castellano <e-lcastellano@autopia.cl>
2022-05-25 00:30:26 -0400

    inital commit

---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import type { NextPage } from 'next';
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
+
+import { Layout } from '../components/layouts';
+import { EntryList, NewEntry } from '../components/ui';
+
+const HomePage: NextPage = () => {
+  return (
+    <Layout title='Home - OpenJira'>
+      
+      <Grid container spacing={ 2 }>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="Pendientes" />
+
+            {/* Agregar una nueva entrada */}
+            {/* Listado de las entradas */}
+            <NewEntry />
+            <EntryList status='pending'/>
+
+
+          </Card>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="En Progreso" />
+            <EntryList status='in-progress' />
+          </Card>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="Completadas" />
+            <EntryList status='finished' />
+          </Card>
+        </Grid>
+
+
+      </Grid>
+
+    </Layout>
+  )
+}
+
+export default HomePage;
